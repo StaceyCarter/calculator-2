5435cf6feb914595d10bbd0fb1aeef8eb2587182
Hackbright Student <no-reply@hackbrightacademy.com>
2019-04-10 15:20:57 -0700

    Added token function

---
+++ calculator.py
@@ -0,0 +1,19 @@
+"""A prefix-notation calculator.
+
+Using the arithmetic.py file from Calculator Part 1, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+def token_string(sen):
+    """ tokenizing the input"""
+    tokens = sen.split(" ")
+    return tokens
+
+token_string("cat dog mouse")
+
+# while True:
+    
+
+# Your code goes here
