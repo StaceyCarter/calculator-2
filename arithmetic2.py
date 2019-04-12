fbd6efbcb4ee19f828a2e228849398545a9ed335
quakerface <stak1903@gmail.com>
2019-04-12 04:03:21 +0000

    convert numbers to floats function implemented

---
+++ arithmetic2.py
@@ -0,0 +1,60 @@
+"""Math functions for calculator."""
+import functools
+
+def add(nums):
+    """Return the sum of the two inputs."""
+
+    return sum(nums)
+
+
+def subtract(num1, num2):
+    """Return the second number subtracted from the first."""
+
+    return num1 - num2
+
+
+def multiply(num1, num2):
+    """Multiply the two inputs together."""
+
+    return num1 * num2
+
+
+def divide(num1, num2):
+    """Divide the first input by the second, returning a floating point."""
+
+    return num1 / num2
+
+
+def square(num1):
+    """Return the square of the input."""
+
+    # Needs only one argument
+
+    return num1 * num1
+
+
+def cube(num1):
+    """Return the cube of the input."""
+
+    # Needs only one argument
+
+    return num1 * num1 * num1
+
+
+def power(num1, num2):
+    """Raise num1 to the power of num and return the value."""
+
+    return num1 ** num2  # ** = exponent operator
+
+
+def mod(num1, num2):
+    """Return the remainder of num / num2."""
+
+    return num1 % num2
+
+
+
+li = [1,2,3,4,5,6]
+
+
+print(functools.reduce(lambda a,b : a+b, li))
--- calculator.py
+++ calculator.py
@@ -4,7 +4,8 @@ Using the arithmetic.py file from Calculator Part 1, create the
 calculator program yourself in this file.
 """
 
-from arithmetic import *
+from arithmetic2 import *
+from functools import reduce
 
 # def token_string(sen):
 #     """ tokenizing the input"""
@@ -19,28 +20,35 @@ def run_calculator():
         tokens = user_input.split(" ")
         if tokens[0] == "q":
             break
+
         elif tokens[0] == "+":
-            print(add(float(tokens[1]), float(tokens[2])))
-        elif tokens[0] == "-":
-            print(subtract(float(tokens[1]), float(tokens[2])))
-        elif tokens[0] == "*":
-            print(multiply(float(tokens[1]), float(tokens[2])))
-        elif tokens[0] == "/":
-            print(divide(float(tokens[1]), float(tokens[2])))
-        elif tokens[0] == "square":
-            print(square(float(tokens[1])))
-        elif tokens[0] == "cube":
-            print(cube(float(tokens[1])))
-        elif tokens[0] == "pow":
-            print(power(float(tokens[1]), float(tokens[2])))
-        elif tokens[0] == "mod":
-            print(mod(float(tokens[1]), float(tokens[2])))
+            print(add(tokens[1:]))
+        # elif tokens[0] == "-":
+        #     print(subtract(float(tokens[1]), float(tokens[2])))
+        # elif tokens[0] == "*":
+        #     print(multiply(float(tokens[1]), float(tokens[2])))
+        # elif tokens[0] == "/":
+        #     print(divide(float(tokens[1]), float(tokens[2])))
+        # elif tokens[0] == "square":
+        #     print(square(float(tokens[1])))
+        # elif tokens[0] == "cube":
+        #     print(cube(float(tokens[1])))
+        # elif tokens[0] == "pow":
+        #     print(power(float(tokens[1]), float(tokens[2])))
+        # elif tokens[0] == "mod":
+        #     print(mod(float(tokens[1]), float(tokens[2])))
             
 
+def convert_nums_to_floats(li):
+    """List -> List. Takes in tokenized input and converts everything into floats
+    """
+    floatified = []
+    for number in li:
+        floatified.append(float(number))
+    return floatified
+
+convert_nums_to_floats(['1', '2', '3.5'])
 
 run_calculator()
 
-# while True:
-    
 
-# Your code goes here
